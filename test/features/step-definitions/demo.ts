301db0c007bd5d77b1e49b34f59a3cde657e580a
Bharat tulsani <bharat.tulsani@hp.com>
2024-03-29 09:26:26 +0530

    29March 2024

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -20,6 +20,37 @@ Then(/^Url should match (.*)$/, async function (ExpectedUrl) {
     console.log(`Expected Url: ${ExpectedUrl}`);
     let url = await browser.getUrl();
     chai.expect(url).to.equal(ExpectedUrl);
-
 });
 
+// Web Interactions
+Given(/^A web page is opened$/, async function () {
+    await browser.url("https://the-internet.herokuapp.com/inputs");
+    await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
+    await browser.maximizeWindow();
+}
+);
+When(/^Perform web Interaction$/, async function () {
+    // let ele = await $("input[type='number']");
+    // await ele.setValue("12345");
+    // await browser.pause(3000);
+
+
+
+    // 1.Input box
+    // Actions
+    // i.Type into Input box
+    // ii.Clear the field and type or just addvalue
+    // iii.Click and Type
+    // iv.slow typing
+
+    let num = 123456;
+    let strNum = num.toString();
+    let ele = await $("input[type='number']");
+    await ele.click();
+    for (let i = 0; i < strNum.length; i++) {
+        let charStr = strNum.charAt(i);
+        await browser.pause(1000);
+        await browser.keys(charStr);
+    }
+    await browser.debug();
+});
