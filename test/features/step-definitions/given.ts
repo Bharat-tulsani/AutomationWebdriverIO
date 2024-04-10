7507b0122f261cc5d138c34db7cb263d7c091e3a
Bharat tulsani <bharat.tulsani@hp.com>
2024-04-10 23:11:31 +0530

    10 APRIL

--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -6,9 +6,30 @@ Given(/^Login to inventory web app$/, async function () {
     await browser.url("https://www.saucedemo.com/v1/index.html");
     await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
     await browser.maximizeWindow();
-    await $(`#user-name`).setValue(`standard_user`);
-    await $(`#password`).setValue(`secret_sauce`);
-    await $(`#login-button`).click();
-    await browser.pause(5000);
+    try {
+        await $(`#user-nam`).setValue(`standard_user`);
+        await $(`#password`).setValue(`secret_sauce`);
+        await $(`#login-button`).click();
+    } catch (err) {
+        console.log(`Error in first login.Retrying...`);
+        await browser.refresh();
+        await browser.pause(5000);
+        await $(`#user-name`).setValue(`standard_user`);
+        await $(`#password`).setValue(`secret_sauce`);
+        await $(`#login-button`).click();
+    }
+    await browser.back();
+    await browser.pause(3000);
+    await browser.forward();
+    await browser.pause(3000);
+
+
+    //Login with another
+    // await browser.reloadSession();
+    // await browser.url("https://www.saucedemo.com/v1/index.html");
+    // await $(`#user-name`).setValue(`problem_user`);
+    // await $(`#password`).setValue(`secret_sauce`);
+    // await $(`#login-button`).click();
+    // await browser.pause(5000);
 
 });
