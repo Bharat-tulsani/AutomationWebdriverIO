07df75b499bc0c7e3282781b5743b508073d152a
Bharat tulsani <bharat.tulsani@hp.com>
2024-04-11 11:35:16 +0530

    add pck

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -156,7 +156,7 @@ export const config: Options.Testrunner = {
         // <boolean> fail if there are any undefined or pending steps
         strict: false,
         // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tags: '@demo',
+        tags: '',
         // <number> timeout for step definitions
         timeout: 60000,
         // <boolean> Enable this config to treat undefined definitions as warnings.
